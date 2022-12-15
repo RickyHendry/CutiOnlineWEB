51c06c53bc3dd7d76fb0f62980f2b2bea808de32
deneb-dev <deneb288@gmail.com>
2022-12-15 21:41:00 +0800

    2.1

--- CutiOnlineWEB/Startup.cs
+++ CutiOnlineWEB/Startup.cs
@@ -69,7 +69,7 @@ namespace CutiOnlineWEB
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
