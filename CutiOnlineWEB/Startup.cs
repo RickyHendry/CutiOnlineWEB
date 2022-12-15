b8a5cfc04c65e6d31ab73108c6eccfe1ee0bb5e7
Hendri <rickyhendry05@gmail.com>
2022-12-15 20:16:21 +0800

    2.0

--- CutiOnlineWEB/Startup.cs
+++ CutiOnlineWEB/Startup.cs
@@ -69,7 +69,7 @@ namespace CutiOnlineWEB
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Account}/{action=login}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
